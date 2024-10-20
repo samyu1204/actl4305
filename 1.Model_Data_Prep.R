fe23f22b7d705a7c3efcd3b223076a6699b8319e
richietiono <richie03tiono@gmail.com>
2024-10-20 15:23:27 +1100

    tidy the code a bit (will fix SA2 mapping soon)

--- 1.Model_Data_Prep.R
+++ 1.Model_Data_Prep.R
@@ -27,11 +27,12 @@ UNSW_claims_data =
   UNSW_claims_data %>%
   filter(tenure >= 0, total_claim_amount>0)
 
+### checking - delete later
 zero_claim_paid <- UNSW_claims_data %>%
   filter(claim_paid <= 0)
+#View(zero_claim_paid)
+#UNSW_earned_data %>% filter(exposure_id == "exposure_11489811-f986-4ef2-9090-46db3f423860")
 
-
-UNSW_earned_data %>% filter(exposure_id == "exposure_11489811-f986-4ef2-9090-46db3f423860")
 
 ## removing totally duplicated row
 ## get distinct  
@@ -39,9 +40,12 @@ UNSW_claims_data = #Removing Duplicate Rows in UNSW Claims Data
   UNSW_claims_data %>%
   distinct()
 
-##look at the duplicated row
+
+##look at the duplicated row - delete later
 duplicated_claim_row = UNSW_claims_data[duplicated(UNSW_claims_data),]
 
+
+##### checking - delete later
 UNSW_claims_data %>% filter(claim_id %in% duplicated_claim_row$claim_id)
 #Investigating duplicated claim id
 n=table(UNSW_claims_data$claim_id)
@@ -94,6 +98,9 @@ Claim_Volume_Vs_Exposure_ID = #Table of Exposure Ids with their respective claim
 
 par(mfrow = c(2, 2))
 
+######################### delete above later
+
+
 #UNSW Earned Data
 
 ##Cleaning the data     
@@ -117,6 +124,7 @@ UNSW_earned_data$quote_date= as.Date(UNSW_earned_data$quote_date, origin = "1970
 UNSW_earned_data$quote_time_group= as.factor(UNSW_earned_data$quote_time_group)
 UNSW_earned_data$nb_postcode= as.factor(UNSW_earned_data$nb_postcode)
 
+
 ###Removing useless row number column
 UNSW_earned_data = 
   UNSW_earned_data %>%
@@ -124,6 +132,7 @@ UNSW_earned_data =
 
 ## check for duplicate
 sum(duplicated(UNSW_earned_data))
+
 
 ### check for num of unique exposure id
 length(unique(UNSW_earned_data$exposure_id))
@@ -135,7 +144,8 @@ exposure <- UNSW_earned_data %>%
   group_by(exposure_id) %>%
   summarise(Total_Earned = sum(earned_units))
 
-## make exposure id collapse into 
+
+## make exposure id collapse into one row only
 #Ensuring that each row in UNSW_earned_data contains a unique exposure_id
 collapsed_UNSW_earned_data = #Earned Data with No Repeated Exposure IDs
   UNSW_earned_data %>%
@@ -146,8 +156,8 @@ collapsed_UNSW_earned_data <- left_join(collapsed_UNSW_earned_data, exposure, by
 
 ## refund for cancellation before 4 days?
 zero_earned_units <- UNSW_earned_data %>% filter(earned_units == 0)
-
 ## if zero total earned units then also zero claim number
+
 
 #Removing negative tenures and 0 claim_paid for covered already paid
 ## keep not paid cover for now
@@ -166,6 +176,7 @@ claim_per_exposure_id = UNSW_claims_data %>%
   group_by(exposure_id) %>%
   summarise(claim_nb = n(), Total_claim_amount = sum(total_claim_amount),
             Total_claim_paid = sum(claim_paid), )
+
 
 ### Aggregate UNSW_earned_data and claim data (claim nb, claim paid and total amount)
 
@@ -196,8 +207,27 @@ for (col in colnames(combined_data)) {
 }
 
 # =================================================================================
-# Merge SA2 code onto it
+# Merge SA2 code onto it (needs to be fixed bcs same postcode has many suburbs)
 sa2_mapping <- read_excel("data/sa2_mapping.xlsx")
+sa2_mapping$nb_postcode <- as.factor(sa2_mapping$nb_postcode)
 names(combined_data)
-combined_data = left_join(sa2_mapping, "SA2_CODE", by = c("nb_postcode"))
+combined_data_sa2 = left_join(combined_data, sa2_mapping, "SA2_CODE", by = c("nb_postcode"))
 
+
+### ignore for now
+## row 1 and 5409 has many-to-many relationship
+
+combined_data_sa2[1,c("nb_postcode", "nb_suburb", "SA2_CODE", "nb_state")]
+combined_data_sa2[5409,c("nb_postcode", "nb_suburb", "SA2_CODE", "nb_state")]
+
+## manually resolve many-to-many relationship between nb_postcode 
+## (resolve with manually checking suburb name)
+
+# for Row 1, suburb name = Sunshine West, so SA_2 code = 213011338
+combined_data_sa2[1,c("SA2_CODE")] <- 213011338
+
+# for Row 5409, suburb name = Hamilton (in QLD), so SA_2 code = 305031124
+combined_data_sa2[5409,c("SA2_CODE")] <- 305031124
+
+##
+combined_data <- combined_data_sa2
